8b46727838e7e26090aae427f919f6217248e517
evita.sivakova <evita.sivakova@gmail.com>
2018-02-20 12:38:41 +0200

    Added test for reader routes

---
+++ tests/Controller/reader/LibraryControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evita.sivakova
+ * Date: 2/20/2018
+ * Time: 11:21 AM
+ */
+
+namespace App\Tests\Controller\reader;
+
+
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LibraryControllerTest extends WebTestCase
+{
+    /** @var Client */
+    private $client;
+
+    public function setUp()
+    {
+        $this->client = static::createClient([],[
+            'PHP_AUTH_USER' => 'reader',
+            'PHP_AUTH_PW' => 'kitten',
+        ]);
+    }
+
+    public function testReaderRoutesAreSuccessful()
+    {
+        $this->client->request('GET', '/catalog/books/1/reserve');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $this->client->request('GET', '/catalog/books/1/toggle-favorite');
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+    }
+}
